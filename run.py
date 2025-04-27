ee3027e01f301d40ce935cc8e36eb1408134fc0a
BeiGai <1438570332@qq.com>
2025-04-27 16:35:18 +0800

    项目运行入口

--- run.py
+++ run.py
@@ -4,4 +4,12 @@
 # @Author  : BeiGai
 # @File    : run.py
 # @Software: win10 Tensorflow1.13.1 python3.6.3
-print("hello python")
+import time
+import pytest
+import os
+
+if __name__ == '__main__':
+    pytest.main()
+    time.sleep(3)
+    os.system('allure generate ./allure-results -o ./allure-report --clean')
+    os.system('allure open ./allure-report')
