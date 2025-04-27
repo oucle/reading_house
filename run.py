58b17034bc47cd9b78d6aef2b83dda5a8004e375
BeiGai <1438570332@qq.com>
2025-04-27 11:15:33 +0800

    Git上传测试

---
+++ run.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2025/4/27 11:12
+# @Author  : BeiGai
+# @File    : run.py
+# @Software: win10 Tensorflow1.13.1 python3.6.3
+print("hello python")
